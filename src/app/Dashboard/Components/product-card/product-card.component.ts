622ab028330cece9b5f9bb5dc7f221f14d4c75a8
Uday Ahamindrakar <udayahamindrakar4441@gmail.com>
2025-03-06 13:20:07 +0530

    Dashbaord version one

---
+++ src/app/Dashboard/Components/product-card/product-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../product';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: false,
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css',
+})
+export class ProductCardComponent {
+  @Input() products: { image: string; title: string; description: string }[] =
+    [];
+  // @Input() product: Product[] = [];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainDashboardComponent } from './Dashboard/Components/main-dashboard/main-dashboard.component';
 
+// #####################  Material imports  #####################
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ProductCardComponent } from './Dashboard/Components/product-card/product-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MainDashboardComponent, ProductCardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
   ],
-  providers: [
-    provideClientHydration(withEventReplay())
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration(withEventReplay())],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
